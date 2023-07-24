d931a3d1c1fff44fac05e87d86cbb442a62f1faa
konpeeyush <piyushksingh2589@gmail.com>
2023-07-24 10:48:01 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,12 +8,10 @@ const Home = () => {
 
             <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row ">
                 <div className="flex flex-col justify-center h-full ">
-                    <h2 className="text-4xl sm:text-7xl font-bold text-white">I'm a Full Stack Developer</h2>
+                    <h2 className="text-4xl sm:text-7xl font-bold text-white">I'm a Full Stack Developer 🚀</h2>
 
                     <p className="text-gray-500 py-4 max-w-md">
-                        I'm a Full Stack Developer with a passion for building
-                        beautiful, functional, and accessible websites and
-                        applications.
+                    I craft immersive digital experiences that captivate users from the first click. With pixel-perfect frontend designs and powerful backend functionalities, I create web applications that leave a lasting impression. Let's build something incredible together. 🚀✨
                     </p>
 
                     <div>
